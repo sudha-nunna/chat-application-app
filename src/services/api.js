301a000b4af9f0034aa7721743183973826b34bd
PravallikaDeepu <mpravallika1606@gmail.com>
2026-07-27 17:20:55 +0530

    deploy changes

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 //   baseURL: "http://localhost:5000/api",
 // });
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 console.log("API base URL:", import.meta.env.VITE_API_URL);
 
